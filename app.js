3e1ad887f4be73fea16fd09af18a3868ffc190c4
Obaid-ur-rehman12 <163607702+Obaid-ur-rehman12@users.noreply.github.com>
2024-03-22 15:41:31 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,94 @@
+// Q no 1
+
+
+// var multidimensionalarr = [
+//     [ ],
+//     [ ],
+//     [ ]
+// ]
+// document.write(multidimensionalarr)
+
+
+
+// Q no 2
+
+// var arr = [
+//     [0, 1, 2, 3],
+//     [1, 0, 1, 2],
+//     [2, 1, 0, 1]
+
+// ];
+
+// for(var a = 0; a < 3; a++){
+//     for(var b = 0; b < 4; b++){
+
+//         document.write(arr[a][b] +" ");
+//     }
+//     document.write("</br>")
+// }
+
+
+
+
+// Q no 3
+// for(i =1; i <= 10; i++){
+//     console.log(i)
+// }
+
+
+// Q no 4
+
+// var tablenum = prompt("enter table number");
+// var tablelen = prompt("enter table length");
+
+// for(var i = 1; i <= tablelen; i++){
+//     document.write(tablenum +"x"+ i +"=" +tablenum*i +"</br>" )
+// }
+
+
+
+
+// Q no 5
+
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+// for (var i = 0; i < fruits.length; i++) {
+//     document.write(fruits[i] + "</br>"); 
+// }
+
+// Q no 6
+
+
+  
+   document.write("<b>Counting:</b> ");
+   for (var i = 1; i <= 15; i++) {
+     document.write(i + ", ");
+   }
+   document.write("<br>");
+  
+
+   document.write("<b>Reverse counting:</b> ");
+   for (var i = 10; i >= 1; i--) {
+     document.write(i + ", ");
+   }
+   document.write("<br>");
+  
+  
+   document.write("<b>Even:</b> ");
+   for (var i = 0; i <= 20; i += 2) {
+     document.write(i + ", ");
+   }
+   document.write("<br>");
+  
+
+   document.write("<b>Odd:</b> ");
+   for (var i = 1; i <= 19; i += 2) {
+     document.write(i + ", ");
+   }
+   document.write("<br>");
+  
+  
+   document.write("<b>Series:</b> ");
+   for (var i = 2; i <= 20; i += 2) {
+     document.write(i + "k, ");
+   }
+

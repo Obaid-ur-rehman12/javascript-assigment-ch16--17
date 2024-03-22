c5fbf91c29c245fcf864b0b1abef71dbc31cb64a
Obaid-ur-rehman12 <163607702+Obaid-ur-rehman12@users.noreply.github.com>
2024-03-22 04:24:08 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,144 @@
+      
+
+    //  function checkCharacterType(character) {
+    //     var charCode = character;
+      
+    //  if (charCode >= 0 && charCode <= 57) {
+    //     console.log(character + " is a number.");
+    // } else if (charCode >= 65 && charCode <= 90) {
+    //      console.log(character + " is an uppercase letter.");
+    //  } else if (charCode >= 97 && charCode <= 122) {
+    //      console.log(character + " is a lowercase letter.");
+    //  } else {
+    //      console.log(character + " is not a valid character.");
+    //   }
+    // }
+
+    // const input = prompt("Enter a character:");
+    // checkCharacterType(input); 
+
+                                        // Q no 2
+
+
+    // function compareIntegers(num1, num2) {
+    //     if (num1 > num2) {
+    //         console.log(num1 + " is larger than " + num2);
+    //     } else if (num2 > num1) {
+    //         console.log(num2 + " is larger than " + num1);
+    //     } else {
+    //         console.log("Both numbers are equal.");
+    //     }
+    // }
+    
+    
+    // const firstNumber = parseInt(prompt("Enter the first integer"));
+    // const secondNumber = parseInt(prompt("Enter the second integer"));
+    // compareIntegers(firstNumber, secondNumber);
+    
+
+                                        //    Q no 3
+
+
+
+    // function checkNumber(num) {
+    //     if (num > 0) {
+    //         console.log(num + " is a positive number.");
+    //     } else if (num < 0) {
+    //         console.log(num + " is a negative number.");
+    //     } else {
+    //         console.log("The number is zero.");
+    //     }
+    // }
+    
+    
+    // const input = (prompt("Enter a number:"));
+    // checkNumber(input);
+
+
+                                    //  Q no 4
+
+    // var greeting;
+    // var hour = 13;
+    // if (hour < 18) {
+    //     greeting = "Good day";
+    // } else {
+    //     greeting = "Good evening";
+    // }
+    
+
+                                            //  Q no 6
+
+
+
+
+    // function getTimePeriod() {
+        
+    //     let time = (prompt("Enter the time in 24-hour clock format (e.g., 1900 for 7pm)"));
+    
+        
+    //     if (time >= 0 && time < 600) {
+    //         console.log("Good night.");
+    //     } else if (time >= 600 && time < 1200) {
+    //         console.log("Good morning.");
+    //     } else if (time >= 1200 && time < 1700) {
+    //         console.log("Good afternoon.");
+    //     } else if (time >= 1700 && time < 2100) {
+    //         console.log("Good evening?");
+    //     } else if (time >= 2100 && time < 2400) {
+    //         console.log("Good night.");
+    //     } else {
+    //         console.log("Invalid input. Please enter a time between 0000 and 2359.");
+    //     }
+    // }
+    
+    
+    // getTimePeriod()
+
+                                                //   CH 14 to 16
+
+
+
+                                                // Q no 1
+
+
+    // var studentNamesLiteral = [];
+
+               
+
+    //                                                // Q no 2
+      
+    //   var studentNamesObject = new Array();
+                                              
+                  
+
+                                                //  Q no 3
+                                            
+    // var numbersArray = [1, 2, 3, 4, 5];
+
+           
+    
+                                            //   Q no 4
+    
+                                                      
+    //   var stringsArray = ["apple", "banana", "orange"];
+
+                                                //   Q no 5
+
+    // let booleanArray = [true, false, true, false];
+
+                                            //   Q no 6
+                                                
+    //  let educationQualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+    //  console.log(educationQualifications[0])
+    //  console.log(educationQualifications[1])
+    //  console.log(educationQualifications[2])
+    //  console.log(educationQualifications[3])
+    //  console.log(educationQualifications[4])
+    //  console.log(educationQualifications[5])
+    //  console.log(educationQualifications[6])
+    //  console.log(educationQualifications[7])
+                                                   
+
+
+    
